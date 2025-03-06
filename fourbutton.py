4580cc5db5df8cbda1f420b57b1b9081b752c6a5
_MadNess <86233076+jjentaa@users.noreply.github.com>
2025-03-06 23:36:07 +0700

    add default pin

--- fourbutton.py
+++ fourbutton.py
@@ -4,7 +4,7 @@ import ssd1306
 from machine import ADC, Pin
 
 class FourButton:
-    def __init__(self, adc_pin1, adc_pin2, adc_pin3, adc_pin4):
+    def __init__(self, adc_pin1=4, adc_pin2=5, adc_pin3=9, adc_pin4=10):
         self.button1 = Pin(adc_pin1, Pin.IN, Pin.PULL_UP) # red
         self.button2 = Pin(adc_pin2, Pin.IN, Pin.PULL_UP) # yellow
         self.button3 = Pin(adc_pin3, Pin.IN, Pin.PULL_UP) # green
