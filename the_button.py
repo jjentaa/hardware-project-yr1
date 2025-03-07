c375f8196a249057871f6f4f5e6e8831194bdad2
Patiph4t <134424515+mou2548@users.noreply.github.com>
2025-03-07 16:41:17 +0700

    Update to latest version

--- the_button.py
+++ the_button.py
@@ -1,28 +1,38 @@
 from machine import Pin, I2C, PWM, ADC
 import random, ssd1306, time, esp32_s3
-
-def current_ms():
-    return time.time_ns() // 1000000
+from complexbutton import ComplexButton
 
 def check_button(last_state):
     if esp.sw.value() == 0 and esp.sw.value() != last_state:
         return True
     return False
 
+def display_center(text, y):
+    esp.oled.text(text,60-len(text)*3,y,1)
+
+def display_time(sec):
+    minute = sec//60
+    sec %= 60
+    sec = round(sec, 3)
+    tm.tm.clear()
+    tm.tm.show(f'{minute:02}{sec:02}', 7)
+
 # using default address 0x3C
 esp = esp32_s3.ESP32_S3(r=42, y=41, g=40, ldr=4, sw=2, sda=48, scl=47, PWM_FREQ=5000)
+tm = ComplexButton(10, 12, 11)
 
 
-TIME = 60 # ---------------- GENERAL BOMB TIME
+TIME = 100 # ---------------- GENERAL BOMB TIME
 TIME_PRECISION = 0.1
 STRIKE = 0
 
 
 last_state = esp.sw.value()
-check_time = current_ms()
-last_time = current_ms()
+check_time = time.ticks_ms()
+last_time = time.ticks_ms()
 
 while True:
+    display_time(TIME)
     no_mistake = True
     sudden = False
     hold = False
@@ -43,12 +53,13 @@ while True:
         esp.green(255)
     
     print(text)
-    # esp.oled.ellipse(64,32,r,r,1,0) # --------------------- DISPLAY BUTTON WITH WORD IN OLED
-    # esp.oled.text(text,60-len(text)*3,30,1)
-    # esp.oled.show()
+    esp.oled.fill(0)
+    esp.oled.ellipse(64,32,r,r,1,0) # --------------------- DISPLAY BUTTON WITH WORD IN OLED
+    display_center(text, 30)
+    esp.oled.show()
     
     if text == 'Abort':
-        if rand in [0, 2]: # ----------- If 0, 2
+        if rand in (0, 2): # ----------- If Red, Green
             hold = True
                      
         else:
@@ -56,58 +67,76 @@ while True:
         
     
     elif text == 'Defuse':
-        if rand == 1: # ----------- If 1
+        if rand == 1: # ----------- If Yellow
             hold = True
   
         else:
             sudden = True
     
     elif text == 'Hold':
-        sudden = True
+        if rand == 2: # ------- If Green
+            hold = True
+        else:
+            sudden = True
     
     elif text == 'Press':
-        hold = True         
+        if rand in (0, 1): # ------ If Red, Yellow
+            hold = True
+        else:
+            sudden = True
     
     elif text == 'Touch':
-        if rand == 1:
+        if rand == 1:	# ------- If Yellow
             hold = True
         else:
             sudden = True
         
     
     if sudden:
+        print("Sudden")
         while esp.sw.value() == 1:
-            check_time = current_ms()
-            last_time = current_ms()
-        while esp.sw.value() == 0:
-            if current_ms() - check_time >= TIME_PRECISION * 1000:
+            last_time = time.ticks_ms()
+            if time.ticks_ms() - check_time >= TIME_PRECISION * 1000:
                 TIME -= TIME_PRECISION
-                check_time = current_ms()
-        if current_ms() - last_time >= 500:
+                check_time = time.ticks_ms()
+                display_time(TIME)
+        while esp.sw.value() == 0:
+            if time.ticks_ms() - check_time >= TIME_PRECISION * 1000:
+                TIME -= TIME_PRECISION
+                check_time = time.ticks_ms()
+                display_time(TIME)
+        if time.ticks_ms() - last_time >= 500:
             no_mistake = False
     
     if hold:
+        print("Hold")
         wait_time = random.randint(3, 3)*1000
         while esp.sw.value() == 1:
-            check_time = current_ms()
-            last_time = current_ms()
-        while current_ms() - last_time <= wait_time: # -- Wrong If released between wait time
-            if current_ms() - check_time >= TIME_PRECISION * 1000:
+            last_time = time.ticks_ms()
+            if time.ticks_ms() - check_time >= TIME_PRECISION * 1000:
                 TIME -= TIME_PRECISION
-                check_time = current_ms()
-#                 print(TIME, esp.sw.value())
+                check_time = time.ticks_ms()
+                display_time(TIME)
+        while time.ticks_ms() - last_time <= wait_time: # -- Wrong If released between wait time
+            if time.ticks_ms() - check_time >= TIME_PRECISION * 1000:
+                TIME -= TIME_PRECISION
+                check_time = time.ticks_ms()
+                display_time(TIME)
             if esp.sw.value() == 1:
                 no_mistake = False
                 break
         else:
             no_mistake = True
-            print("Release the button")
-            last_time = current_ms()
-            while current_ms() - last_time <= 1500: # -- Wrong If not released in 1500 ms
-                if current_ms() - check_time >= TIME_PRECISION * 1000:
+            esp.oled.fill(0)
+            display_center("Release", 15)
+            display_center("the button", 30)
+            esp.oled.show()
+            last_time = time.ticks_ms()
+            while time.ticks_ms() - last_time <= 1500: # -- Wrong If not released in 1500 ms
+                if time.ticks_ms() - check_time >= TIME_PRECISION * 1000:
                     TIME -= TIME_PRECISION
-                    check_time = current_ms()
-#                     print(TIME, esp.sw.value())
+                    check_time = time.ticks_ms()
+                    display_time(TIME)
                 if esp.sw.value() == 1:
                     break
             else:
@@ -116,8 +145,16 @@ while True:
     if no_mistake:
         break
     else:
-        print("Not Passed, Try Again")
+        esp.oled.fill(0)
+        display_center("Failed", 15)
+        display_center("Try again.", 30)
+        esp.oled.show()
+        time.sleep(2)
+        TIME -= 2
         STRIKE += 1
+        display_time(TIME)
 
-print("Passed")
+esp.oled.fill(0)
+display_center("Passed", 30)
+esp.oled.show()
 
