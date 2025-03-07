6b7a5d3d8fb1e009da871d79eba7d4ac77c48682
Patiph4t <134424515+mou2548@users.noreply.github.com>
2025-03-07 16:42:09 +0700

    Makes it compatible with jane's

--- joystick.py
+++ joystick.py
@@ -1,11 +1,13 @@
 from machine import ADC,Pin
+import time
 
 class Joystick:
     
-    def __init__(self, x_pin, y_pin, sw_pin, y_invert=True, sw_invert=True):
+    def __init__(self, x_pin=16, y_pin=17, sw_pin=15, x_invert=False, y_invert=False, sw_invert=True):
         self.x = ADC(Pin(x_pin, Pin.IN))
         self.y = ADC(Pin(y_pin, Pin.IN))
         self.sw = Pin(sw_pin, Pin.IN, Pin.PULL_UP)
+        self.x_invert = x_invert
         self.y_invert = y_invert
         self.sw_invert = sw_invert
         
@@ -25,7 +27,24 @@ class Joystick:
         self.xVal = round((self.xVal - 2047)/ 2048)
         self.yVal = round((self.yVal - 2047)/ 2048)
         
+        if self.x_invert:
+            self.xVal *= -1
+        
         if self.y_invert:
             self.yVal *= -1
         
         return (self.xVal, self.yVal, self.swVal)
+    
+    def direction(self):
+        x_direction = {
+            0: 'Center',
+            -1: 'Left',
+            1: 'Right'
+        }
+        y_direction = {
+            0: 'Center',
+            -1: 'Down',
+            1: 'Up'
+        }
+        x, y, sw = self.read()
+        return (x_direction[x], y_direction[y])
