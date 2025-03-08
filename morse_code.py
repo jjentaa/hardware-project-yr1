838cffdd9ac5456a9d585f325ba5d2a38af04293
Patiph4t <134424515+mou2548@users.noreply.github.com>
2025-03-09 04:20:47 +0700

    Update morse_code.py

--- morse_code.py
+++ morse_code.py
@@ -1,16 +1,32 @@
 from machine import Pin, I2C, PWM, ADC
-import ssd1306, time, esp32_s3, random
+import ssd1306, time, random
 import array
-from joystick import Joystick
-from complexbutton import ComplexButton
 
-esp = esp32_s3.ESP32_S3(r=42, y=41, g=40, ldr=4, sw=2, sda=48, scl=47, PWM_FREQ=5000)
-tm = ComplexButton(10, 12, 11)
+from esp32_s3 import ESP32_S3
+from joystick import Joystick
+
+# Initialize components
+esp = ESP32_S3(r=42, y=41, g=40, ldr=4, sw=2, sda=48, scl=47, PWM_FREQ=5000, board_id=2)
 joy = Joystick(x_invert=True)
+
+if esp.board_id == 1:
+    from complexbutton import ComplexButton
+    
+    complexbutton = ComplexButton(10, 12, 11)
+    tm = complexbutton.tm
+    
+elif esp.board_id == 2:
+    from tm1637 import TM1637
+    from fourbutton import FourButton, FourLeds
+    
+    four_buttons = FourButton(5, 6, 18, 8)
+    four_leds = FourLeds(r=9, y=10, g=45, b=21)
+    tm = TM1637(12, 11)
 
 TIME = 100  # General bomb time
 TIME_PRECISION = 0.1
 STRIKE = 0
+STRIKE_LIMIT = 3
 SUBMIT_COOLDOWN = 500  # 500ms cooldown for submit button
 last_submit_time = 0
 
@@ -29,8 +45,8 @@ word_list = list(word_dict.keys())
 freq_list = list(word_dict.values())
 sorted_freq_list = sorted(word_dict.values())
 
-option = random.randint(0, len(word_dict) - 1)
-i = random.randint(0, len(freq_list) - 1)
+option = random.randint(0, 10000) % len(word_dict)
+i = random.randint(0, 10000) % len(word_dict)
 print(option, word_list[option], word_dict[word_list[option]])
 check_time = time.ticks_ms()
 last_time = time.ticks_ms()
@@ -41,25 +57,41 @@ is_beeping = False
 morse_timer = time.ticks_ms()
 joystick_cooldown = time.ticks_ms()
 
-def display_time(sec):
-    minute = sec // 60
-    sec %= 60
-    sec = round(sec, 3)
-    tm.tm.clear()
-    tm.tm.show(f'{minute:02}{sec:02}', 7)
+def display_center(text, y=30):
+    esp.oled.text(text,60-len(text)*3,y,1)
+    
+def update_timer():
+    global TIME, check_time
+    if time.ticks_ms() - check_time >= TIME_PRECISION * 1000:
+        TIME -= TIME_PRECISION
+        check_time = time.ticks_ms()
+        display_time(TIME)
 
-def update_oled():
+def display_time(sec):
+    minute = int(sec // 60)
+    sec %= 60
+    tm.clear()
+    if esp.board_id == 1:
+        tm.show(f'{minute:02.0f}{sec:02.1f}', 7)
+    elif esp.board_id == 2:
+        tm.show(f'{minute:02.0f}{sec:02.0f}', 1)
+        
+def update_oled(side=None):
     select_freq = sorted_freq_list[i]
     esp.oled.fill(0)
     esp.oled.rect(5, 3, 120, 15, 1)
     esp.oled.rect(20, 28, 90, 30, 1)
-    esp.oled.text(f"{select_freq} MHz", 29, 38, 1)
+    esp.oled.text(f"{select_freq:.2f} MHz", 29, 38, 1)
     rect_center = int((select_freq - 38) * 115)
     esp.oled.fill_rect(rect_center - 1, 1, rect_center // 16 + 1, 19, 1)
     left_triangle = array.array('i', [13, 31, 13, 51, 3, 41])
     right_triangle = array.array('i', [115, 31, 115, 51, 125, 41])
     esp.oled.poly(0, 0, left_triangle, 1)
     esp.oled.poly(0, 0, right_triangle, 1)
+    if side == 'left':
+        esp.oled.poly(0, 0, left_triangle, 1, 1)
+    if side == 'right':
+        esp.oled.poly(0, 0, right_triangle, 1, 1)
     esp.oled.show()
 
 def update_morse():
@@ -91,21 +123,20 @@ display_time(TIME)
 update_oled()
 
 while TIME > 0:
-    if time.ticks_ms() - check_time >= TIME_PRECISION * 1000:
-        TIME -= TIME_PRECISION
-        check_time = time.ticks_ms()
-        display_time(TIME)
+    update_timer()
     
     if time.ticks_ms() - joystick_cooldown > 200:
         joy_x = joy.read()[0]
         if joy_x == -1 and i > 0:
             i -= 1
-            update_oled()
+            update_oled('left')
             joystick_cooldown = time.ticks_ms()
         elif joy_x == 1 and i < len(freq_list) - 1:
             i += 1
-            update_oled()
+            update_oled('right')
             joystick_cooldown = time.ticks_ms()
+        elif joy_x == 0:
+            update_oled()
     
     if joy.read()[2]:
         if time.ticks_ms() - last_submit_time > SUBMIT_COOLDOWN:
@@ -122,19 +153,21 @@ while TIME > 0:
                 # Wait briefly so player can see the message
                 error_start = time.ticks_ms()
                 while time.ticks_ms() - error_start < 1000:  # Show for 1 second
-                    # Keep updating the timer during this time
-                    if time.ticks_ms() - check_time >= TIME_PRECISION * 1000:
-                        TIME -= TIME_PRECISION
-                        check_time = time.ticks_ms()
-                        display_time(TIME)
+                    update_timer()
                     pass
-                if STRIKE >= 3:
+                if STRIKE >= STRIKE_LIMIT:
                     TIME = 0
                     
                 update_oled()
     
     update_morse()
     
-esp.oled.fill(0)
-esp.oled.text("Game Over", 30, 30, 1)
-esp.oled.show()
+if TIME > 0:
+    esp.oled.fill(0)
+    display_center("Passed", 30)
+    esp.oled.show()
+else:
+    esp.oled.fill(0)
+    display_center("Game over.", 30)
+    esp.oled.show()
+
